1e66a5fb2201ef15d780c7671550da0c79b4a1bb
Ming Rutar <mingx.rutar@intel.com>
2017-02-16 18:03:18 -0800

    added pidstat cvs

--- run_exec.py
+++ run_exec.py
@@ -9,45 +9,96 @@ import platform
 import sqlite3
 
 PIDSTAT_INTERVAL = 1        #1 sec
+TILE_WORKSPACE = "/mnt/app_hdd1/scratch/mingperf/tiledb-ws/"
 
 DEVNULL = open(os.devnull, 'wb', 0)
-time_format='-f "cmd:%C,elapse_sec:%e,CPU_sec:%P,major_pf:%F,minor_pf:%R,v_cs:%w,fs_input:%I,fs_output:%O,iv_cs:%c,exit_sts:%x"'
+time_format="-f cmd:%C,elapse_sec:%e,CPU_sec:%P,major_pf:%F,minor_pf:%R,v_cs:%w,fs_input:%I,fs_output:%O,iv_cs:%c,exit_sts:%x"
 working_path = os.getcwd()
 
 def measure_more( cmd, log_path ) :
   logfile = os.path.join(log_path, "ps_%s.log" % platform.node())
-  pidstatlog = open(pidstatlog, 'w', 0)
+  pidstatlog = open(logfile, 'w', 0)
   time_lines_count = 1     # how many lines /usr/bin/time produces
-  pexec = Popen(['/usr/bin/time', time_format] + cmd, shell=False, stdout=DEVNULL, stderr=PIPE) 
+  theExecCmd = ['/usr/bin/time', time_format] + cmd
+  print("executing command %s " % (str(theExecCmd)))
+  pexec = Popen(theExecCmd, shell=False, stdout=DEVNULL, stderr=PIPE) 
   pstat = Popen(['/usr/bin/pidstat', '-dl', '1', '-p', '%s' % pexec.pid ], stdout=pidstatlog, stderr=pidstatlog) 
   print("pid_pexec=%s, pid_pstat=%s" % (pexec.pid, pstat.pid))
 
   with pexec.stderr:
     qexec = deque(iter(pexec.stderr.readline, b''), maxlen=time_lines_count)
+    print("pexec exited .. pid_pexec=%s" % (pexec.pid))
   rc = pexec.wait()
+ 
+  pidstatlog.close()
   pstat.kill()
   retStr = b''.join(qexec).decode().strip() 
-  print( retStr )
+  print( "retStr=%s" % retStr )
   name_val = dict(x.split(':') for x in retStr.split(','))
-  return name_val, pidstatlog
+  return name_val, logfile
 
-INSERT_TIME ="INSERT INTO time_result (run_id, log_text, partition_1_size, db_size) \
- VALUES (%d, %s, %d, %d)"
-def save_time_log(run_id, time_output) :
-    db_conn = sqlite3.connect('genomicsdb_loader.db')
+INSERT_TIME ="INSERT INTO time_result (run_id, log_text, partition_1_size, db_size, pidstat_path) \
+ VALUES (%s, \"%s\", %d, %d, %s)"
+def save_time_log(working_dir, run_id, time_output, pidstat_cvs) :
+    db_instance = os.path.join(working_dir, 'genomicsdb_loader.db')
+    db_conn = sqlite3.connect(db_instance)
     mycursor = db_conn.cursor()
     #TODO check db size
-    stmt = INSERT_TIME % (run_id, str(time_output, 0, 0))
+    stmt = INSERT_TIME % (run_id, str(time_output), 0, 0, pidstat_cvs)
+    print("stmt=%s" % stmt)
     mycursor.execute(stmt)
     db_conn.commit()
     db_conn.close()
+
+def run_pre_test(working_dir, tiledb_root) :
+  pre_test = os.path.join(working_dir, 'prelaunch_chec.bash')
+  if os.path.isfile(pre_test) :
+    pid = Popen([pre_test, tiledb_root], stdout=PIPE, stderr=PIPE)
+    out, err = pid.communicate()
+    print(out)
+    return  pid.returncode == 0
+  else:
+    return None
+
+def pidstat2cvs(ifile, of_prefix) :
+    def __to_epoch(timestrs) :
+        ''' timestrs = [ 'hh:mm:ss', 'A|PM' ] '''
+        thetime = '%s %s %s' % (time.strftime('%d%m%y'), timestrs[0], timestrs[1])
+        epochtime = time.mktime(time.strptime(thetime, '%d%m%y %I:%M:%S %p'))
+        return epochtime
+    with open(ifile, 'r') as fd:
+        lines = fd.readlines()
+    linelist = [ x.split()  for x in lines[3:] ]
+    # find all unique (UID, PID)
+    proc_set = set(map(tuple, [ x[2:4] for x in linelist] )) 
+    for pp in proc_set: 
+        # pid : [( ts, rs, writePerSec, ccws) ]  
+        pid_output[pp] = [ (str(__to_epoch(x[0:2])), x[4:7][0], x[4:7][1], x[4:7][2] ) for x in linelist if tuple(x[2:4]) == pp ]
+
+    for key, val in pid_output.items() :
+        ofile = "%s_%s.cvs" % (of_prefix, key[1])
+        ofd = open(ofile, 'w')
+        ofd.write("ts, rs, ws,ccws\n")
+        [ ofd.write("%s\n" % ','.join(data)  ) for data in val ]
+        ofd.close()
 
 if __name__ == '__main__' :
     jsonfl_path = sys.argv[1]
     with open(jsonfl_path, 'r') as ldf:
         exec_json = json.load(ldf)
-    pprint(exec_json)
-    stat_path = os.path.join(os.path.abspath(jsonfl_path), 'stats')
-    time_nval, pidstatlog = measure_more(exec_json['cmd'], stat_path)
-    save_time_log(time_nval)
-    #TODO make pidstatlog to csv file 
+
+    working_dir = os.path.join( os.path.dirname(jsonfl_path), os.pardir)
+
+    rc = run_pre_test( working_dir, TILE_WORKSPACE)
+    print("retcode from run_pre_test = %s" % rc)
+    if rc:  
+      stat_path = os.path.join(working_dir, 'stats')
+      if not os.path.isdir(stat_path) :
+        os.mkdir(stat_path)
+      target_cmd = [ str(x.rstrip()) for x in  exec_json['cmd'].split(' ') ]
+      # print('target_cmd=%s' % target_cmd)
+      time_nval, pidstatlog = measure_more(target_cmd, stat_path)
+      cvsfile = pidstatlog[:-4]
+      pidstat2cvs(pidstatlog, cvsfile)
+      save_time_log(working_dir, exec_json['run_id'], time_nval, cvsfile)
+    
